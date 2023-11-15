66793c070d94988351907ec01a7b2691b44e4cff
dtfigueiredo <dt.figueiredo90@gmail.com>
2023-11-15 17:07:49 -0300

    custom fonts and colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,42 @@
+import { GithubLogo, LinkedinLogo } from '@phosphor-icons/react';
+
 export function App() {
 	return (
-		<div className='bg-zinc-700'>
+		<div className='bg-amethyst-700'>
 			<div className='h-screen flex flex-col'>
-				<header className='bg-zinc-300'>HEADER</header>
+				<header className='bg-amethyst-950 font-poppins text-2xl text-center text-amethyst-50 py-5 shadow shadow-amethyst-500'>
+					Online Password Generator
+				</header>
 
-				<main className='flex-1 flex justify-between gap-4 w-4/5 max-w-6xl mx-auto mt-8'>
-					<section className='w-full bg-zinc-400'>APP</section>
-					<aside className='w-[320px] bg-zinc-400'>ASIDE</aside>
+				<main className='flex-1 flex justify-between gap-4 w-4/5 max-w-6xl mx-auto mt-16'>
+					<section className='w-full bg-amethyst-300 font-noto p-6 rounded-xl shadow-sm shadow-tidal-200'>
+						<h1 className='font-poppins text-xl font-bold text-center pt-6'>GENERATOR</h1>
+					</section>
+
+					<aside className='w-[480px] bg-amethyst-300 font-noto p-6 rounded-xl shadow-sm shadow-tidal-200'>
+						<h1 className='font-poppins text-xl font-bold text-center pt-6'>SAVED PASSWORDS</h1>
+					</aside>
 				</main>
 
-				<footer className='mt-8 bg-zinc-300'>FOOTER</footer>
+				<footer className='bg-amethyst-950 font-poppins text-center text-amethyst-50 py-2 mt-16'>
+					<p className='flex justify-center items-center gap-2'>
+						desenvolvido por <strong>Daniel Figueiredo</strong>
+						<a
+							href='https://github.com/dtfigueiredo'
+							target='_blank'
+							rel='noopener noreferrer'
+							className='transition-all hover:text-amethyst-600'>
+							<GithubLogo size={24} />
+						</a>
+						<a
+							href='https://www.linkedin.com/in/dtfigueiredo/'
+							target='_blank'
+							rel='noopener noreferrer'
+							className='transition-all hover:text-amethyst-600'>
+							<LinkedinLogo size={24} />
+						</a>
+					</p>
+				</footer>
 			</div>
 		</div>
 	);
