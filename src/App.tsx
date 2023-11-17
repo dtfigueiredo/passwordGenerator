9abee037c795a3c48c3fb3fa7f7abdadf3a4f9ea
dtfigueiredo <dt.figueiredo90@gmail.com>
2023-11-17 18:40:44 -0300

    vh fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,16 @@ import { Generator } from './components/Generator';
 
 export function App() {
 	return (
-		<section className='bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-layout-darkgray to-zinc-900'>
-			<div className='h-screen md:h-screen flex flex-col justify-center items-center'>
-				<p className='flex items-end gap-2 text-3xl'>
-					<span className='text-layout-mediumgray'>
-						<Lock size={36} />
-					</span>
-					<span className='text-layout-orange'>Password</span>
-					<span className='font-bold text-layout-lightgray'>Generator</span>
-				</p>
+		<section className='h-screen flex flex-col justify-center items-center bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-layout-darkgray to-zinc-900'>
+			<p className='flex items-end gap-2 text-3xl'>
+				<span className='text-layout-mediumgray'>
+					<Lock size={36} />
+				</span>
+				<span className='text-layout-orange'>Password</span>
+				<span className='font-bold text-layout-lightgray'>Generator</span>
+			</p>
 
-				<Generator />
-			</div>
+			<Generator />
 		</section>
 	);
 }
